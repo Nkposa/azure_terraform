896f21b0527e04e99cda8fc4b77a2f1d5b766c84
NaveenP <Naveen.Posa@thomsonreuters.com>
2025-11-22 11:42:48 +0530

    Update app name to youtube-naveen

--- main.tf
+++ main.tf
@@ -1,20 +1,19 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+data "azurerm_resource_group" "example" {
+  name = var.resource_group_name
 }
 
 resource "azurerm_service_plan" "example" {
   name                = var.app_service_plan_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.example.name
   os_type             = "Linux"
   sku_name            = var.app_service_plan_sku
 }
 
 resource "azurerm_linux_web_app" "example" {
   name                = var.app_service_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.example.name
   service_plan_id     = azurerm_service_plan.example.id
 
   site_config {
--- terraform.tfvars
+++ terraform.tfvars
@@ -2,14 +2,14 @@
 location = "southindia"
 
 # Resource group configuration
-resource_group_name = "youtubeclonegroup"
+resource_group_name = "myResourceGroup"
 
 # App Service Plan configuration
-app_service_plan_name = "ASP-myResourceGroup-9881"
-app_service_plan_sku = "B2"
+app_service_plan_name = "ASP-myResourceGroup-9468"
+app_service_plan_sku = "B1"
 
 # App Service configuration
-app_service_name = "naveenyoutubeclone"
+app_service_name = "youtube-naveen"
 
 # Runtime configuration
-node_version = "22-lts"
+node_version = "20-lts"
